1924b8e1e0db4ffbe1188c0525ff86337055567e
SanctaGee <tonygabito@gmail.com>
2026-02-25 17:16:15 +0100

    feat: add multer middleware for project image uploads

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,17 +1,17 @@
-
 const express = require("express")
 const router = express.Router()
 const projectController = require("../controllers/projectController")
 const { checkJWT } = require("../utilities/")
+const upload = require("../middleware/upload") 
 
 // Public Routes (anyone can access it)
 router.get("/", projectController.getAllProjects)
 router.get("/featured", projectController.getFeaturedProjects)
 router.get("/:id", projectController.getProjectById)
 
-// Admin only (Proected Routes)
-router.post("/", checkJWT, projectController.addProject)
-router.put("/:id", checkJWT, projectController.updateProject)
+// Admin only (Protected Routes) - WITH IMAGE UPLOAD
+router.post("/", checkJWT, upload.single("project_image"), projectController.addProject)
+router.put("/:id", checkJWT, upload.single("project_image"), projectController.updateProject)
 router.delete("/:id", checkJWT, projectController.deleteProject)
 
 module.exports = router
